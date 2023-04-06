06610171a0fd4a0eacbf8d588648f337d81277d5
roman-razvan-30121 <roman_razvan03@yahoo.com>
2023-04-06 16:52:55 +0300

    Delete isp-lab-9-master directory

--- isp-lab-9-master/src/main/java/isp/lab9/exercise2/SensorController.java
+++
@@ -1,9 +0,0 @@
-package isp.lab9.exercise2;
-
-public class SensorController implements Observer{
-    @Override
-    public void update(Object event) {
-        System.out.println("sensor type: "+ ((String)event).split(" ")[0]);
-    }
-
-}
